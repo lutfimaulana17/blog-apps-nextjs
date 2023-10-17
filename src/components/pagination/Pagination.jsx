bb983c79b5b00b2773229c187b3c7bf6500f3319
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-10-17 08:00:25 +0700

    fix pagination for category query params

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,22 +1,43 @@
 "use client"
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './pagination.module.css'
-import {useRouter} from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 const Pagination = ({page, hasPrev, hasNext}) => {
     const router = useRouter()
+    const searchParams = useSearchParams()
+    const nextPage = page + 1;
+    const prevPage = page - 1;
+
+    const handlePrevButtonClick = () => {
+        const cat = searchParams.get('cat')
+        if (cat) {
+            router.push(`?cat=${cat}&page=${prevPage}`)
+        } else {
+            router.push(`?page=${prevPage}`)
+        }
+    };
+
+    const handleNextButtonClick = () => {
+        const cat = searchParams.get('cat')
+        if (cat) {
+            router.push(`?cat=${cat}&page=${nextPage}`)
+        } else {
+            router.push(`?page=${nextPage}`)
+        }
+    };
 
     return (
         <div className={styles.container}>
             <button
                 className={styles.button}
                 disabled={!hasPrev}
-                onClick={() => router.push(`?page=${page - 1}`)}>Previous</button>
+                onClick={() => handlePrevButtonClick()}>Previous</button>
             <button
                 className={styles.button}
                 disabled={!hasNext}
-                onClick={() => router.push(`?page=${page + 1}`)}>Next</button>
+                onClick={() => handleNextButtonClick()}>Next</button>
         </div>
     )
 }
